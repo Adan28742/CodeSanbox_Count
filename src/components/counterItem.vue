fb8988b6ee41f3631c70bf0e64b4861c7942d17c
Mesias Adan <110131766+Adan28742@users.noreply.github.com>
2023-05-23 08:26:38 -0500

    Initial commit

---
+++ src/components/counterItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <div>
+      <h4>El numero actual es ...</h4>
+      <h1>{{ count }}</h1>
+      <button>+</button>
+      <button>-</button>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "counterItem",
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
